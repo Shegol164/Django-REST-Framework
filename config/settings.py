eec7e5041527ffeb18f5ca851cc7b4a9c5e5bee3
Pavel <shegol164@mail.ru>
2025-09-05 15:01:32 +0400

    homework_35.2 исправленный

--- config/settings.py
+++ config/settings.py
@@ -90,8 +90,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-IS_TESTING = any('test' in arg for arg in sys.argv)
 IS_GITHUB_ACTIONS = os.getenv('GITHUB_ACTIONS') == 'true'
+
+# Правильная проверка тестового режима
+IS_TESTING = any('test' in arg for arg in sys.argv)
 
 if IS_TESTING or IS_GITHUB_ACTIONS:
     DATABASES = {
@@ -107,7 +109,7 @@ else:
             'NAME': os.getenv('DB_NAME'),
             'USER': os.getenv('DB_USER'),
             'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST', 'localhost'),
+            'HOST': os.getenv('DB_HOST', 'db'),
             'PORT': os.getenv('DB_PORT', '5432'),
             'OPTIONS': {
                 'connect_timeout': 5,
